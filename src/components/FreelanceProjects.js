b850b42f3b112682248f6b08907d8950abce2897
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-04-26 22:34:59 +0530

    Added coponents and routes

---
+++ src/components/FreelanceProjects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FreelanceProjects = () => {
+  return (
+    <div>FreelanceProjects</div>
+  )
+}
+
+export default FreelanceProjects
---
+++ src/components/SkillsSections.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SkillsSections = () => {
+  return (
+    <div>SkillsSections</div>
+  )
+}
+
+export default SkillsSections
